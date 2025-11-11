3abeab194f027fa7b7858ab1734618081bc216ab
Masaki I. <m1117@aol.jp>
2025-11-11 15:45:02 +0900

    update

--- test-offset/vm.c
+++ test-offset/vm.c
@@ -29,6 +29,7 @@ int main() {
         for (int i = 0; i < 5; i++) {
             printf("%lu", ptr[offset+i * (PAGE_SIZE / sizeof(unsigned long))]);
         }
+        puts("");
         offset++;
     }
 
