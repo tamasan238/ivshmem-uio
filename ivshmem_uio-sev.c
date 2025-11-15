28b9281a47ee591208f80aff16bb2195962c14f9
Masaki I. <m1117@aol.jp>
2025-11-15 18:13:33 +0900

    update

--- ivshmem_uio-sev.c
+++ ivshmem_uio-sev.c
@@ -155,10 +155,10 @@ static int ivshmem_mmap(struct uio_info *info, struct vm_area_struct *vma)
         return -EINVAL;
 
     vma->vm_ops = &uio_physical_vm_ops;
-    vma->vm_page_prot = pgprot_noncached(vma->vm_page_prot);
-	// vma->vm_page_prot = pgprot_writecombine(vma->vm_page_prot);
-	
-	vma->vm_flags |= VM_IO | VM_PFNMAP | VM_DONTEXPAND | VM_DONTDUMP;
+    // vma->vm_page_prot = pgprot_noncached(vma->vm_page_prot);
+	vma->vm_page_prot = pgprot_writecombine(vma->vm_page_prot);
+
+	// vma->vm_flags |= VM_IO | VM_PFNMAP | VM_DONTEXPAND | VM_DONTDUMP;
 
 
       ret = remap_pfn_range(vma, vma->vm_start,
